822f8c55185973b906233e27b3a4b38a29d67a1b
Ganesh Rakate <3976235+ganeshr5@users.noreply.github.com>
2020-10-30 18:01:50 +0530

    Updated predict.py

---
+++ src/predict.py
@@ -0,0 +1,12 @@
+# Prediction
+
+import os
+import pandas as pd
+import numpy as np
+from sklearn import ensemble
+from sklearn import preprocessing
+from sklearn import metrics
+
+import joblib
+
+from . import dispatcher
