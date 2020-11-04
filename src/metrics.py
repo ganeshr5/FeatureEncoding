b95eaeab09d9ee6aca52980d9054586d0049592d
Ganesh Rakate <3976235+ganeshr5@users.noreply.github.com>
2020-11-04 20:18:45 +0530

    Update metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -1,2 +1,10 @@
 # Classification Metrics
 # Evaluate the performance of classifiers
+
+from sklearn import metrics as met
+
+# Create a class ClassMetrics 
+# Define the following accuracy measures
+# Accuracy, AUC, logloss, precision, recall, f1score
+
+
